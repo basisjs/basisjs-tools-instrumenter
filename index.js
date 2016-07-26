b486dfd1b76554e03798e6e08021a3f8b33f7442
Roman Dvornov <rdvornov@gmail.com>
2016-07-27 01:48:27 +0300

    DRY: use most part of runtime API from babel plugin

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+var fs = require('fs');
+
 module.exports = {
   server: function(api, options) {
     var registratorName = options.registratorName || '$devinfo';
-    var injectScript = require('fs')
-      .readFileSync(__dirname + '/js-runtime.js', 'utf-8')
-      .replace('__ref__', registratorName);
+    var injectScript = 
+      fs.readFileSync(require.resolve('babel-plugin-source-wrapper/runtime.js'), 'utf-8') +
+      fs.readFileSync(__dirname + '/js-runtime.js', 'utf-8');
 
     var processorConfig = {
       registratorName: registratorName,
       blackbox: Array.isArray(options.blackbox) ? options.blackbox : null,
-      injectScriptFilename: api.addVirtualFile('runtime.js', injectScript)
+      injectScriptFilename: api.addVirtualFile('runtime.js',
+        injectScript.replace(/DEVINFO_API_NAME/g, '"' + registratorName + '"')
+      )
     };
 
     api.addPreprocessor('.html', require('./html-process')(processorConfig));
--- js-runtime.js
+++ js-runtime.js
@@ -1,41 +1,14 @@
-window.__ref__ = (function(){
-  if (typeof WeakMap !== 'function') {
-    console.info('basisjs-tools-instrumenter is not available');
-    return function(ref) {
-      return ref;
-    };
+window[DEVINFO_API_NAME].fixSourceOffset = function(source, offset) {
+  if (offset && typeof btoa == 'function') {
+    offset = ':' + (offset || 0) + ',';
+    while (offset.length % 3) offset += ' ';  // get rid of base64 padding
+    source = source
+      //{"version":3,"sections":[{"offset":{"line:0,"
+      .replace(/(eyJ2ZXJzaW9uIjozLCJzZWN0aW9ucyI6W3sib2Zmc2V0Ijp7ImxpbmUi)OjAs/, '$1' + btoa(offset))
+      // cut off ?instr
+      .replace(/P2luc3Ry/, '');
   }
 
-  var map = new WeakMap();
-  var result = function(ref, data, force) {
-    if (data && ref && (typeof ref === 'object' || typeof ref === 'function')) {
-      var curData = map.get(ref);
-      if (force || !curData || (curData.blackbox && !data.blackbox)) {
-        map.set(ref, data);
-      }
-    }
+  return source;
+};
 
-    return ref;
-  };
-
-  result.set = result;
-  result.get = function(ref) {
-    return (ref && map.get(ref)) || undefined;
-  };
-
-  result.fixSourceOffset = function(source, offset) {
-    if (offset && typeof btoa == 'function') {
-      offset = ':' + (offset || 0) + ',';
-      while (offset.length % 3) offset += ' ';  // get rid of base64 padding
-      source = source
-        //{"version":3,"sections":[{"offset":{"line:0,"
-        .replace(/(eyJ2ZXJzaW9uIjozLCJzZWN0aW9ucyI6W3sib2Zmc2V0Ijp7ImxpbmUi)OjAs/, '$1' + btoa(offset))
-        // cut off ?instr
-        .replace(/P2luc3Ry/, '');
-    }
-
-    return source;
-  };
-
-  return result;
-})();
